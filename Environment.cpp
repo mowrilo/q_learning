82ced11b9c63cf275edd34b309444aa5c77b6e0b
Murilo <murilovfm@gmail.com>
2018-07-05 17:20:41 -0300

    done with the environment

---
+++ Environment.cpp
@@ -0,0 +1,130 @@
+#include "Environment.hpp"
+
+using namespace std;
+
+Environment::Environment(){
+//    srand(2); // initialize seed
+}
+
+vector<int> Environment::read_map(string filename){
+    ifstream f(filename);
+    int lin,col;
+    f >> lin >> col;
+    string line;
+    this->n_lin = lin;
+    this->n_col = col;
+    this->map = new string[lin];    
+    int n_line = 0;
+    vector<int> nterminal_states;
+    while(getline(f,line)){
+        this->map[n_line] = line;
+        for (int n_column=0; n_column<col; n_column++){
+            if (line[n_column] == '-'){
+                int this_state = XYtoN(n_line,n_column);
+                cout << "state: " << nterminal_states.size() << "\n";
+                nterminal_states.push_back(this_state);
+            }
+        }
+        n_line++;
+    }
+    //this->nonterminal_states = nterminal_states;
+    return nterminal_states;
+}
+
+// x is the number of the line, y is the number of the column
+int Environment::XYtoN(int x, int y){
+    return x* this->n_col + y;
+}
+
+pair<int,int> Environment::NtoXY(int state){
+   int x = state/this->n_col;
+   int y = state % this->n_col;
+   return make_pair(x,y);
+}
+
+char Environment::query_state(int state){//int x, int y){
+    if (state < 0 || state > (this->n_lin*this->n_col - 1))    return '#';
+    pair<int,int> xy = NtoXY(state);
+    return this->map[xy.first][xy.second];
+}
+
+// Returns the triple (reward, next state, isTerminal)
+// action maps: 
+//      up: 0
+//      right: 1
+//      down: 2
+//      left: 3
+pair<int,pair<int,int> > Environment::take_action(int act){
+    int rew = 0;
+    pair<int,int> new_state = current_state;
+    int isTerminal = 0;
+    if (act == 0){
+        new_state.first--;
+        int new_state_int = XYtoN(new_state.first, new_state.second);
+        if (query_state(new_state_int) == '#'){
+            new_state = current_state;
+        }
+        new_state_int = XYtoN(new_state.first, new_state.second);
+        rew = get_reward(new_state_int);
+        if (rew == 10 || rew == -10)    isTerminal = 1;
+    }
+    else if (act == 1){
+        new_state.second++;
+        int new_state_int = XYtoN(new_state.first, new_state.second);
+        if (query_state(new_state_int) == '#'){
+            new_state = current_state;
+        }
+        new_state_int = XYtoN(new_state.first, new_state.second);
+        rew = get_reward(new_state_int);
+        if (rew == 10 || rew == -10)    isTerminal = 1;
+    }
+    else if (act == 2){
+        new_state.first++;
+        int new_state_int = XYtoN(new_state.first, new_state.second);
+        if (query_state(new_state_int) == '#'){
+            new_state = current_state;
+        }
+        new_state_int = XYtoN(new_state.first, new_state.second);
+        rew = get_reward(new_state_int);
+        if (rew == 10 || rew == -10)    isTerminal = 1;
+    }
+    else if (act == 3){
+        new_state.second--;
+        int new_state_int = XYtoN(new_state.first, new_state.second);
+        if (query_state(new_state_int) == '#'){
+            new_state = current_state;
+        }
+        new_state_int = XYtoN(new_state.first, new_state.second);
+        rew = get_reward(new_state_int);
+        if (rew == 10 || rew == -10)    isTerminal = 1;
+    }
+    else{
+        cout << "You took a forbidden action! You should be penalized for that!\n";
+        rew = -100;
+    }
+    this->current_state = new_state;
+    int next_state = XYtoN(new_state.first, new_state.second);
+    return make_pair(isTerminal,make_pair(rew, next_state));
+}
+
+int Environment::get_reward(int state){
+    char state_obs = query_state(state);
+    if (state_obs == '-')   return -1;
+    else if (state_obs == '0')  return 10;
+    else if (state_obs == '&')  return -10;
+    return 0;
+}
+
+// Generates a random state for the agent to start
+int Environment::reset(){
+    int num_of_nonterminal = nonterminal_states.size();
+    int new_state = rand() % num_of_nonterminal;
+    new_state = nonterminal_states[new_state];
+    this->current_state = NtoXY(new_state);
+    return new_state;
+}
+
+Environment::~Environment(){
+    delete[] this->map;
+}
+
--- Environment.hpp
+++ Environment.hpp
@@ -1,15 +1,29 @@
+#include <cstdlib>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <utility>
+#include <tuple>
+#include <vector>
+
+using namespace std;
 
 class Environment{
     public:
         Environment();
-        void read_map(string filename);
-        char query_state(int x, int y);
-        pair<int,int> take_action(int act);
+        // read map returns a list of possible states!
+        vector<int> read_map(string filename);
+        pair<int,int> NtoXY(int state);
+        int XYtoN(int x, int y);
+        char query_state(int state);//int x, int y);
+        pair<int,pair<int,int> > take_action(int act);
+        int reset();
+        int get_reward(int state);
         ~Environment();
     private:
-        char** map;
+        int n_lin;
+        int n_col;
+        vector<int> nonterminal_states;
+        string* map;
         pair<int,int> current_state;
-}
+};
